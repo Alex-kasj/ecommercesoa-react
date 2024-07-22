b9c5eb1177fe2ae389210fb6ed4e11edf06b6cd2
Alex-kasj <Kasjken@gmail.com>
2024-07-22 00:07:18 -0500

    Updates

---
+++ precache-manifest.b93ad6c9085b4edfd019827ec11e57a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8141d390ee95b4633e6079a87aea8fb",
+    "url": "/ecommercesoa-react-cliente/index.html"
+  },
+  {
+    "revision": "61df10b6cc54bab30402",
+    "url": "/ecommercesoa-react-cliente/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "4edd0b934c9355861e5d",
+    "url": "/ecommercesoa-react-cliente/static/css/main.38228fb1.chunk.css"
+  },
+  {
+    "revision": "61df10b6cc54bab30402",
+    "url": "/ecommercesoa-react-cliente/static/js/2.8d2ec024.chunk.js"
+  },
+  {
+    "revision": "6df8d49e07fe72054483",
+    "url": "/ecommercesoa-react-cliente/static/js/3.8b257cf5.chunk.js"
+  },
+  {
+    "revision": "4edd0b934c9355861e5d",
+    "url": "/ecommercesoa-react-cliente/static/js/main.d0f6bddb.chunk.js"
+  },
+  {
+    "revision": "4708b06bcb0ca10f3e5d",
+    "url": "/ecommercesoa-react-cliente/static/js/runtime~main.39b480ff.js"
+  }
+]);
